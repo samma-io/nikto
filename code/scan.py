63e47dd19da522e97840c4acdd26e8f7de96af8b
Mattias Hemmingsson <matte@elino.se>
2019-06-19 16:37:55 +0200

    Think working nikto

--- code/scan.py
+++ code/scan.py
@@ -17,26 +17,46 @@ def start_scan():
     '''
     Start the nmap scan of the target
     '''
-    process = subprocess.Popen('nikto -h {0}  -p {1} -C all  -output /niktoscan.csv'.format(os.environ['TARGET'],"80,443") , shell=True, stdout=subprocess.PIPE)
+    process = subprocess.Popen('nikto -h {0}  -p {1} -C all  -output /niktoscan.csv'.format(os.environ['TARGET'],"80,443") , shell=True, universal_newlines=True,stdout=subprocess.PIPE)
     for stdout_line in iter(process.stdout.readline, ""):
         print(stdout_line) 
-    process.stdout.close()
+    
     return_code = process.wait()
-    if return_code:
-        raise subprocess.CalledProcessError(return_code, cmd) 
-
+    
+    print("Scanning done")
 
 
 def convert_output():
     '''
     Converts the output to json
     '''
+    
     with open('/niktoscan.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
-            print(row)
-            print(row[0])
-            print(row[0],row[1],row[2],)
+            try: 
+                json_data ={
+                    "host":row[0],
+                    "target":os.environ['TARGET'],
+                    "type":"Nikto",
+                    "ip":row[1],
+                    "port":row[2],
+                    "OSVDB":row[3],
+                    "request": row[4],
+                    "url": row[5],
+                    "finding": row[6]
+                }
+                print(json.dumps(json_data))
+
+            except: 
+
+
+                json_data ={
+                    "target":os.environ['TARGET'],
+                    "type":"Nikto",
+                    "data": row
+                }
+                print(json.dumps(json_data))
 
 
 #Start the scan and get the output
